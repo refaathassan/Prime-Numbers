48bbffcab9ad7ab3aa129fbe0a968567d24e0816
Refaat Hassan <refathassan1289641@gmail.com>
2024-12-25 21:38:28 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+in_number=int(input("please enter your number:   ")) 
+if in_number<=0:print(in_number)
+elif in_number==1:print(in_number)
+else:
+    print(1)
+    for i in range(2,in_number+1):
+            flag=0
+            for j in range(2,i):
+                if i%j==0:
+                    flag=1
+            if flag==0:
+                print(i)
+        
